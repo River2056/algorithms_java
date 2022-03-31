f5aa62b7c2d6d0e1fb8f1e7057b4361b3d068a88
kevin.tung <chen0625tung@gmail.com>
2022-03-31 14:34:12 +0800

    initialize java repo for algorithms

---
+++ src/com/river/datastructure/BinarySearchTree.java
@@ -0,0 +1,43 @@
+package com.river.datastructure;
+
+public class BinarySearchTree {
+
+    private int size;
+    private TreeNode<Integer> root;
+
+    public int length() {
+        return this.size;
+    }
+
+    public TreeNode<Integer> insert(Integer value) {
+        TreeNode<Integer> node = new TreeNode<>(value);
+
+        if (this.root == null) {
+            this.root = node;
+            return node;
+        }
+
+        TreeNode<Integer> current = this.root;
+        while (true) {
+            if (current.getValue() < node.getValue()) {
+                if (current.getRightChild() == null) {
+                    current.setRightChild(node);
+                    this.size++;
+                    return node;
+                }
+                current = current.getRightChild();
+            } else {
+                if (current.getLeftChild() == null) {
+                    current.setLeftChild(node);
+                    this.size++;
+                    return node;
+                }
+                current = current.getLeftChild();
+            }
+        }
+    }
+
+    public String printBST() {
+        return this.root == null ? null : this.root.toString();
+    }
+}
