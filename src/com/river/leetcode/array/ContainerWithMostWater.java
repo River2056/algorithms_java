c85388b4e101c04e0195437597634e5c4074bc5d
kevin.tung <chen0625tung@gmail.com>
2022-04-08 12:51:25 +0800

    + added leetcode:ContainerWithMostWater problem

---
+++ src/com/river/leetcode/array/ContainerWithMostWater.java
@@ -0,0 +1,40 @@
+package com.river.leetcode.array;
+
+public class ContainerWithMostWater {
+    @FunctionalInterface
+    interface CustomFunction {
+        int apply(int[] inputs);
+    }
+
+    public static void printResult(int[] input, int expectedOutput, CustomFunction fn) {
+        int result = fn.apply(input);
+        System.out.printf("result: %s%n", result);
+        if (result != expectedOutput)
+            throw new RuntimeException(
+                    String.format("should be %s, got %s instead", expectedOutput, result));
+    }
+
+    // brute force
+    public static int containerWithMostWaterBrute(int[] heights) {
+        int maxArea = 0;
+        for (int i = 0; i < heights.length; i++) {
+            for (int j = i + 1; j < heights.length; j++) {
+                int height = Math.min(heights[i], heights[j]);
+                int width = j - i;
+                int area = width * height;
+                maxArea = Math.max(maxArea, area);
+            }
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        CustomFunction fn = heights -> containerWithMostWaterBrute(heights);
+        printResult(new int[] {1, 8, 6, 2, 5, 4, 8, 3, 7}, 49, fn);
+        printResult(new int[] {7, 1, 2, 3, 9}, 28, fn);
+        printResult(new int[] {1, 2, 3, 4, 5}, 6, fn);
+    }
+}
+
+
