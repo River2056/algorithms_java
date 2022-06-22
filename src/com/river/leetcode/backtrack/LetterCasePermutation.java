455e3624b055332876100f332a7558bca6f77623
kevin.tung <chen0625tung@gmail.com>
2022-06-22 23:28:10 +0800

    + added problems

---
+++ src/com/river/leetcode/backtrack/LetterCasePermutation.java
@@ -0,0 +1,38 @@
+package com.river.leetcode.backtrack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LetterCasePermutation {
+    public List<String> letterCasePermutation(String s) {
+        List<StringBuilder> ans = new ArrayList<>();
+        ans.add(new StringBuilder());
+        for (int i = 0; i < s.length(); i++) {
+            int length = ans.size();
+            if (Character.isLetter(s.charAt(i))) {
+                for (int j = 0; j < length; j++) {
+                    ans.add(new StringBuilder(ans.get(j)));
+                    ans.get(j).append(Character.toLowerCase(s.charAt(i)));
+                    ans.get(j + length).append(Character.toUpperCase(s.charAt(i)));
+                }
+            } else {
+                for (int j = 0; j < length; j++)
+                    ans.get(j).append(s.charAt(i));
+            }
+        }
+
+        List<String> res = new ArrayList<>();
+        for (StringBuilder sb : ans)
+            res.add(sb.toString());
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        LetterCasePermutation letter = new LetterCasePermutation();
+
+        System.out.printf("1. %s\n", letter.letterCasePermutation("a1b2"));
+        System.out.printf("2. %s\n", letter.letterCasePermutation("3z4"));
+        System.out.printf("3. %s\n", letter.letterCasePermutation("Jw"));
+    }
+}
---
+++ src/com/river/leetcode/dynamicProgramming/CountingBits.java
@@ -0,0 +1,34 @@
+package com.river.leetcode.dynamicProgramming;
+
+import java.util.Arrays;
+
+public class CountingBits {
+    public int[] countBits(int n) {
+        int[] res = new int[n + 1];
+        for (int i = 0; i < res.length; i++) {
+            int count1 = count(i);
+            res[i] = count1;
+        }
+
+        return res;
+    }
+
+    private int count(int n) {
+        int res = 0;
+        while (n != 0) {
+            int r = n % 2;
+            if (r == 1)
+                res++;
+            n /= 2;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        CountingBits cb = new CountingBits();
+
+        System.out.printf("res: %s\n", Arrays.toString(cb.countBits(2)));
+        System.out.printf("res: %s\n", Arrays.toString(cb.countBits(5)));
+    }
+}
